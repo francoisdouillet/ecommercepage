4579c166bed2af2ce866f272b7be303df58ae8a0
Francois Douillet <francois.douillet@sfr.fr>
2022-02-17 09:15:34 +0100

    17february

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 const showCart = document.getElementById("showCart");
 const btnCart = document.getElementById("btnCart");
-console.log(showCart)
-console.log(btnCart)
+showCart.style.display = "none"
+
 btnCart.onclick = function () {
-  if (showCart.style.display !== "none") {
-    showCart.style.display = "none";
-  } else {
+  if (showCart.style.display == "none") {
     showCart.style.display = "block";
+  } else  {
+    showCart.style.display = "none";
   }
 };
